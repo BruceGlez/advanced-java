a9b24a6411f017dfa423146d92fecff6473a4875
BruceGlez <brucegonzalezpeters@hotmail.com>
2020-07-09 12:20:48 -0600

    Changed hello message

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,7 +1,7 @@
-pacakge app;
+package app;
 
 public class App{
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
